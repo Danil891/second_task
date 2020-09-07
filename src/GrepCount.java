7941f402cd6c6d7d5ae2b06a510f2ae5c916b157
Danil <cherepkov.dd@edu.spbstu.ru>
2020-09-08 00:33:32 +0700

    updated gitignore

---
+++ src/GrepCount.java
@@ -0,0 +1,52 @@
+public class GrepCount {
+    private String[] args;
+    private boolean ilogic = false;
+    private boolean vlogic = false;
+    private boolean rlogic = false;
+    private String word;
+    private String filename;
+
+    public GrepCount(String[] args) {
+        this.args = args;
+        int index;
+
+        for (index = 0; index < args.length - 1; index++){
+            switch (args[index]){
+                case "-i":
+                    ilogic = true;
+                    break;
+                case "-v":
+                    vlogic = true;
+                    break;
+                case "-r":
+                    rlogic = true;
+                    break;
+                default:
+                    word = args[index];
+                    break;
+            }
+        }
+        filename = args[args.length - 1];
+
+    }
+
+    public boolean getIlogic() {
+        return ilogic;
+    }
+
+    public boolean getRlogic() {
+        return rlogic;
+    }
+
+    public boolean getVlogic() {
+        return vlogic;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        GrepCount start = new GrepCount(args);
+        boolean ilogic = start.getIlogic();
+        boolean rlogic = start.getRlogic();
+        boolean vlogic = start.getVlogic();
+
+    }
+
+
+}
